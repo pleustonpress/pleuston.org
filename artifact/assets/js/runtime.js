394b4ac880012c7e31496fff6e27e787c6932035
phil616 <phil616@163.com>
2024-10-05 11:43:21 +0800

    update: update js and template res
    update runtime.js for fix search problem, close #9
    update html template to compat
    update runtime.js for impl social icon, close #7
    for same task id equals qa60100, proj blog need to update(already
    update)

---
+++ artifact/assets/js/runtime.js
@@ -0,0 +1,43 @@
+// Task ID: qa60100
+document.getElementById('search-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // trim the query and redirect to search page
+    var query = document.getElementById('query').value.trim();
+    if (query) {
+        window.location.href = 'https://blog.pleuston.org/search/?q=' + query;
+    }else {
+        window.location.href = 'https://blog.pleuston.org/search/';
+    }
+});
+
+// https://github.com/pleustonpress/pleuston.org/issues/7
+document.addEventListener('DOMContentLoaded', function() {
+    var addToFavoritButton = document.getElementById('header-favourites');
+    var EmailButton = document.getElementById('header-email');
+    var instagramButton = document.getElementById('header-instagram');
+    instagramButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        // open instagram page
+        window.location.href = 'https://www.instagram.com/';
+    });
+    EmailButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        // open email client
+        window.location.href ='mailto:help@pleuston.org';
+    });
+    addToFavoritButton.addEventListener('click', function(event) {
+        event.preventDefault(); 
+
+        var url = window.location.href; 
+        var title = document.title; 
+
+        if (window.sidebar && window.sidebar.addPanel) { // Firefox <23
+            window.sidebar.addPanel(title, url, '');
+        } else if (window.external && ('AddFavorite' in window.external)) { // IE
+            window.external.AddFavorite(url, title);
+        } else {
+            alert('Use the Ctrl+D shortcut to add this page to your favorites.');
+        }
+    });
+});
+
